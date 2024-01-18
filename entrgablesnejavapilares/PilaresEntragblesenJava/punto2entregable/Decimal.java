ed91e25b38e7b39cc5a4a587edc763163d01d3f6
Francisco Cordova <129221830+Pac7Ar@users.noreply.github.com>
2024-01-18 17:32:13 -0600

    Add files via upload

---
+++ entrgablesnejavapilares/PilaresEntragblesenJava/punto2entregable/Decimal.java
@@ -0,0 +1,13 @@
+public class Decimal {
+
+public static void main(String[] args) {
+
+    boolean insPuedesAlamacenarDecimales=true;
+
+    boolean insPuedesAlamacenarDecimales =10.12;
+
+    System.out.println("Si el numero es deciaml es verdadero"+insPuedesAlamacenarDecimales); 
+    System.out.println("Si el numero decimal es falso "+insPuedesAlamacenarDecimales);   
+}
+
+}
